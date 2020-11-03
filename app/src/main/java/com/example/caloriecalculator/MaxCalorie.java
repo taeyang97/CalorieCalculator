4dbb0cf5bb93b375954203c2707eb4e7833f03d2
taeyang97 <zlaxodid1@naver.com>
2020-11-04 00:33:50 +0900

    wait..

---
+++ app/src/main/java/com/example/caloriecalculator/MaxCalorie.java
@@ -0,0 +1,14 @@
+package com.example.caloriecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaxCalorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_max_calorie);
+    }
+}
