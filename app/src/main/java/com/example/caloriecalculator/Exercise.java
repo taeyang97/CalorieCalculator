7536809060ba24bc6ec7d58d2d6b7ee09fd866ad
taeyang97 <zlaxodid1@naver.com>
2020-12-02 02:09:32 +0900

    운동 클래스 GridView로 변경

--- app/src/main/java/com/example/caloriecalculator/Exercise.java
+++ app/src/main/java/com/example/caloriecalculator/Exercise.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -12,47 +13,72 @@ import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Exercise extends AppCompatActivity {
-    ImageView Exercise[] = new ImageView[12];
-    int Exercises[] = {R.id.ivExerciseRun, R.id.ivExerciseSoc, R.id.ivExerciseBad,
-            R.id.ivExerciseGol, R.id.ivExerciseHan, R.id.ivExerciseRol,
-            R.id.ivExerciseYan, R.id.ivExerciseTen, R.id.ivExerciseTak,
-            R.id.ivExerciseSno, R.id.ivExerciseSki, R.id.ivExerciseBas};
-    int x,y;
+    GridView gridView;
+    MyAdapter myAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         ActionBar ac = getSupportActionBar();
         ac.hide();
-        x=0;
-        y=0;
+        gridView = (GridView)findViewById(R.id.gridView);
+        myAdapter = new MyAdapter(this);
+        gridView.setAdapter(myAdapter);
 
-        for(int i=0; i<Exercise.length; i++){
-             Exercise[i] = (ImageView)findViewById(Exercises[i]);
-            int index;
-            index = i;
-            Exercise[index].setOnTouchListener(new View.OnTouchListener() {
+    }
+    public class MyAdapter extends BaseAdapter {
+        int Exercises[] = {R.drawable.exerciserun, R.drawable.exercisesoc, R.drawable.exercisebad,
+                R.drawable.exercisegol, R.drawable.exercisehan, R.drawable.exerciserol,
+                R.drawable.exerciseyan, R.drawable.exerciseten, R.drawable.exercisetak,
+                R.drawable.exercisesno, R.drawable.exerciseski, R.drawable.exercisebas};
+        Context context;
+        public MyAdapter(Context context) {
+            this.context=context;
+        }
+
+        @Override
+        public int getCount() {
+            return Exercises.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+            int x = display.getWidth();
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(x/3,(int)(x/4*1.5))); // 뷰 항목마다 크기 선언
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER); // 이미지를 크기안에 중간에 오게 //FIT_XY는 크기 안에 이미지를 꽉차게 하는 것
+            imageView.setPadding(30,30,30,30);
+            imageView.setImageResource(Exercises[position]);
+            imageView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
                     switch (event.getAction()){
                         case MotionEvent.ACTION_DOWN:
-                            x=Exercise[index].getWidth()+20;
-                            y=Exercise[index].getHeight()+20;
-                            Exercise[index].getLayoutParams().width=x;
-                            Exercise[index].getLayoutParams().height=y;
-                            Exercise[index].requestLayout();
+                            imageView.setPadding(15,15,15,15);
                             break;
                         case MotionEvent.ACTION_UP:
-                            x=-20;
-                            y=-20;
-                            Exercise[index].getLayoutParams().width=x;
-                            Exercise[index].getLayoutParams().height=y;
-                            Exercise[index].requestLayout();
+                            imageView.setPadding(30,30,30,30);
                             Intent intent = new Intent(getApplicationContext(),ExerciseLast.class);
                             startActivity(intent);
                             break;
@@ -60,6 +86,7 @@ public class Exercise extends AppCompatActivity {
                     return true;
                 }
             });
+            return imageView;
         }
     }
 }
